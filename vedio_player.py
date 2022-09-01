f945c33658ce4bab5d69bd3f28961395f60fc641
Apurbo Saha <92245030+apurbo-saha@users.noreply.github.com>
2022-09-01 12:19:34 +0600

    Create vedio_player.py

---
+++ vedio_player.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+
+google = pyttsx3.init()
+listener = sr.Recognizer()
+
+
+
+google.say("how can i help you sir")
+google.runAndWait()
+
+
+try:
+   with sr.Microphone() as source:
+       print("listening...")
+       print("must be said : Play")
+       voice = listener.listen(source)
+       command = listener.recognize_google(voice)
+       print(command)
+
+   if "play" in command:
+       play_vedio = command.replace("play", "")
+       print("plaing" + play_vedio + "...")
+       pywhatkit.playonyt(play_vedio)
+
+except:
+    pass
